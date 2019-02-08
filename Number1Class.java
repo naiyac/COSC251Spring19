e375f5dbd3e06f500ff6b979fbd226142117b589
naiyac <34172387+naiyac@users.noreply.github.com>
2019-02-08 04:18:34 -0500

    Update Number1Class.java

--- Number1Class.java
+++ Number1Class.java
@@ -1,5 +1,5 @@
 package Number1;
-
+//Assignment 1 Exercise 1 for Intro to Java class (COSC 251  Sp 19 Sem)
 public class Number1Class {
     public static void main (String [] args){
         System.out.println("*************");
